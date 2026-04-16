99f697bfc840748185f00f438b8731df00f0b483
meiasp <normei210@gmail.com>
2026-04-16 22:48:26 +0700

    Enhance image preview with event listeners
    Refactor image preview functionality and add event listeners for accessibility.

--- js/gallery.js
+++ js/gallery.js
@@ -3,14 +3,32 @@ function upDate(previewPic){
     const box = document.getElementById('image');
     document.getElementById('image').innerHTML = previewPic.alt;
     box.style.backgroundImage = "url('" + previewPic.src + "')";
-    console.log('"' + previewPic.alt + '"' + "showed");
+
 }
 
-	function unDo(){
+function unDo(){
 
     const box = document.getElementById('image');
     box.style.backgroundImage = "url()";
     box.innerHTML = "Hover over an image below to display here.";
-    console.log("mouse leaved");
-		
-	}
+	
+}
+
+const boxImage = document.getElementsByClassName('preview');
+    for (let i = 0; i < boxImage.length; i++){
+        boxImage[i].setAttribute('tabindex','0');
+        boxImage[i].addEventListener('mouseover',function(){
+            upDate(this);
+            console.log("mouse hover works [" + this.alt + "]");
+        });
+        boxImage[i].addEventListener('mouseleave',function(){unDo();});
+        boxImage[i].addEventListener('focus',function(){
+            upDate(this);
+            console.log("tab works [" + this.alt + "]");
+        });
+        boxImage[i].addEventListener('blur',function(){unDo();});
+    }
+    
+for (let i = 0; i < boxImage.length; i++) {
+    console.log("loop run for index:", i);
+}
